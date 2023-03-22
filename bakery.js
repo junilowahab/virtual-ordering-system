d6663cf04b112ec852f26f4b840b3399081ddaeb
junilowahab <junilojoshuawahab@gmail.com>
2023-03-22 14:37:36 +0100

    Added comments

--- bakery.js
+++ bakery.js
@@ -1,19 +1,25 @@
+//Linking the DOM
 const slider = document.querySelector('.image-slider');
 const arrLeft = document.querySelector('.arrow-left');
 const arrRight = document.querySelector('.arrow-right');
 
+//Created an array containing the different background images
 const img = ['brett-jordan-U15Q9FIhsjE-unsplash.jpg', 'keesha-s-kitchen-_y325luZNUs-unsplash.jpg', 'sanju-m-gurung-WX-wxGHFTzA-unsplash.jpg', 'tabitha-turner-PSqT-lQAt7A-unsplash.jpg'];
 
+//This represents the index of the image array
 let id = 0;
 
+//function that shows the different images in each slide
 function slide(id){
     slider.style.backgroundImage = `url(./bakery_images/${img[id]})`
+    //this creates the fadein/fadeout effect
     slider.classList.add(document.querySelector('.image-fade'));
     setTimeout(() => {
         slider.classList.remove(document.querySelector('.image-fade'));
     }, 550);
 }
 
+//This changes index of the image array when right arrow clicked
 arrRight.addEventListener('click', () => {
     id++;
     if(id > 3){
@@ -22,6 +28,7 @@ arrRight.addEventListener('click', () => {
     slide(id);
 });
 
+//This changes index of the image array when left arrow clicked
 arrLeft.addEventListener('click', () => {
     id--;
     if(id < 0){
