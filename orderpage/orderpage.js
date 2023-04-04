97206c77cc95cd215841c8d429df4724b188f128
junilowahab <junilojoshuawahab@gmail.com>
2023-04-04 17:18:05 +0100

    edited the automatic slider

--- orderpage/orderpage.js
+++ orderpage/orderpage.js
@@ -1,17 +1,17 @@
-const body = document.getElementById('body');
-const wallpapers = ['jakub-dziubak-XtUd5SiX464-unsplash.jpg', 'nathan-dumlao-gOn7dKcCWKg-unsplash.jpg', 'puffsyrup.jpg'];
+const wallpapers = ['jakub-dziubak-XtUd5SiX464-unsplash.jpg', 'nathan-dumlao-gOn7dKcCWKg-unsplash.jpg', 'mae-mu-m9pzwmxm2rk-unsplash.jpg', 'joshua-hoehne-30-l5M0v_m8-unsplash.jpg'];
 
 function automaticSlider(){
+    console.log('Page has loaded');
     let id = 0;
     var timer;
 timer = setInterval(() => {
     body.style.backgroundImage = `url(../bakery_images/${wallpapers[id]})`;
     console.log(id);
     id++;
-    if(id === 3){
+    if(id === 4){
         id = 0;
     };
 }, 4000);
 }
 
-body.addEventListener('', automaticSlider)
+window.addEventListener('load', automaticSlider)
