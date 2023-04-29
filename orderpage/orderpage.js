0257ba506e5c98de4964afeb47c56358e0a02ad1
junilowahab <junilojoshuawahab@gmail.com>
2023-04-29 12:35:37 +0100

    created basis in which to add different products to the cart page

--- orderpage/orderpage.js
+++ orderpage/orderpage.js
@@ -49,8 +49,51 @@ let orderData = {};
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         orderData['productName'] = document.getElementById(`${button.getAttribute('id')}-name`).innerText;
-        orderData['productPrice'] = `${document.getElementById(`${button.getAttribute('id')}-price`).innerText} each`;
+        orderData['productPrice'] = `${document.getElementById(`${button.getAttribute('id')}-price`).innerText}`;
     
         console.log(orderData);
     })
 });
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', createCartContent);
+})
+
+function createCartContent(){
+    let cartContents = document.getElementById('cart-contents');
+
+    //created the background of the cart content 
+    let cartContent = document.createElement('div');
+    cartContent.classList.add('cart-content');
+
+    cartContents.appendChild(cartContent);
+
+    //added image of product to the content box
+    let img = document.createElement('img');
+    img.src = '../bakery_images/puff.jpg';
+
+    cartContent.appendChild(img);
+
+    //created container that will contain the name of the product
+    let label = document.createElement('div');
+    label.classList.add('label');
+
+    cartContent.appendChild(label);
+
+    //added name of product and quantity to the label container
+    let cartProductName = document.createElement('h2');
+    cartProductName.textContent = 'Puff Puff';
+
+    let cartProductQuantity = document.createElement('h5');
+    cartProductQuantity.textContent = 'Qty 1';
+
+    label.appendChild(cartProductName);
+    label.appendChild(cartProductQuantity);
+
+    //added pric tag to the cart content box
+    let price = document.createElement('h2');
+    price.textContent = '250.00';
+
+    cartContent.appendChild(price);
+}
